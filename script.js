28a6fcc01a65365d714d098a2a30952d772957cc
where-is-my-mind1 <62612433+where-is-my-mind1@users.noreply.github.com>
2023-01-20 20:26:19 +0300

    Navigation block fix

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let dropdown = document.getElementsByClassName("dropdown-button");
+let i;
+
+for (i = 0; i < dropdown.length; i++) 
+{
+    dropdown[i].addEventListener("click", function() 
+    {
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") 
+        {
+            dropdownContent.style.display = "none";
+        } 
+        else 
+        {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
